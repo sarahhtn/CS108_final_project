72b94a154199b42e5f736376eedec1169490792e
Sarah Nader <snader2@stanford.edu>
2013-11-09 15:13:48 -0800

    added Question Interface and Question classes

---
+++ QuizWebsite/src/quizsite/Question.java
@@ -0,0 +1,15 @@
+package quizsite;
+
+public interface Question {
+
+	/**
+	 * Returns true if answer is correct, false if it's not
+	 * */
+	public boolean checkAnswer();
+	
+	/**
+	 * 
+	 * */
+	
+	
+}
